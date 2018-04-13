9fe98d80e292e7d2da31fa1882cf02d7eb087b48
thecho7 <thecho77@gmail.com>
2018-04-13 17:32:51 +0900

    Update generate_proposal.py

--- generate_proposal.py
+++ generate_proposal.py
@@ -1,1 +1,91 @@
+from model.utils.config import cfg  
+from __future__ import print_function
 
+import numpy as np
+import pdb
+
+# Verify that we compute the same anchors as Shaoqing's matlab implementation:
+#
+#    >> load output/rpn_cachedir/faster_rcnn_VOC2007_ZF_stage1_rpn/anchors.mat
+#    >> anchors
+#
+#    anchors =
+#
+#       -83   -39   100    56
+#      -175   -87   192   104
+#      -359  -183   376   200
+#       -55   -55    72    72
+#      -119  -119   136   136
+#      -247  -247   264   264
+#       -35   -79    52    96
+#       -79  -167    96   184
+#      -167  -343   184   360
+
+#array([[ -83.,  -39.,  100.,   56.],
+#       [-175.,  -87.,  192.,  104.],
+#       [-359., -183.,  376.,  200.],
+#       [ -55.,  -55.,   72.,   72.],
+#       [-119., -119.,  136.,  136.],
+#       [-247., -247.,  264.,  264.],
+#       [ -35.,  -79.,   52.,   96.],
+#       [ -79., -167.,   96.,  184.],
+#       [-167., -343.,  184.,  360.]])
+
+try:
+    xrange          # Python 2
+except NameError:
+    xrange = range  # Python 3
+
+
+def generate_anchors():
+    """
+    Generate anchor (reference) windows by enumerating aspect ratios X
+    scales wrt a reference (0, 100) window.
+    """
+    bbox_default_len = cfg.BBOX_LENGTH
+    bbox_stride = cfg.ANCHOR_STRIDE
+    bbox_scales = cfg.ANCHOR_SCALES
+
+    base_anchor = np.array([1, bbox_default_len]) - 1
+    anchors = np.vstack([_scale_enum(base_anchor[i, :], bbox_scales)
+                         for i in xrange(base_anchor.shape[0])])
+    return anchors
+
+def _whctrs(anchor):
+    """
+    Return width, height, x center, and y center for an anchor (window).
+    """
+
+    w = anchor[1] - anchor[0] + 1
+    x_ctr = anchor[0] + 0.5 * (w - 1)
+    return w, x_ctr
+
+def _mkanchors(ws, hs, x_ctr, y_ctr):
+    """
+    Given a vector of widths (ws) and heights (hs) around a center
+    (x_ctr, y_ctr), output a set of anchors (windows).
+    """
+
+    ws = ws[:, np.newaxis]
+    hs = hs[:, np.newaxis]
+    anchors = np.hstack((x_ctr - 0.5 * (ws - 1),
+                         x_ctr + 0.5 * (ws - 1)))
+    return anchors
+
+def _scale_enum(anchor, scales):
+    """
+    Enumerate a set of anchors for each scale wrt an anchor.
+    """
+
+    w, x_ctr = _whctrs(anchor)
+    ws = w * scales
+    anchors = _mkanchors(ws, x_ctr)
+    return anchors
+
+if __name__ == '__main__':
+    import time
+    t = time.time()
+    a = generate_anchors()
+    print(time.time() - t)
+    print(a)
+    from IPython import embed; embed()
