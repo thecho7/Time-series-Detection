809f909a24aedece27c6eac9dcc4502fe367853f
thecho7 <thecho77@gmail.com>
2018-04-14 15:34:54 +0900

    Update bbox_transform.py

--- bbox_transform.py
+++ bbox_transform.py
@@ -65,60 +65,29 @@ def bbox_transform_batch(ex_rois, gt_rois):
     return targets
 
 def bbox_transform_inv(boxes, deltas, batch_size):
-    widths = boxes[:, :, 2] - boxes[:, :, 0] + 1.0
-    heights = boxes[:, :, 3] - boxes[:, :, 1] + 1.0
-    ctr_x = boxes[:, :, 0] + 0.5 * widths
-    ctr_y = boxes[:, :, 1] + 0.5 * heights
+    length = boxes[:, :, 1] - boxes[:, :, 0] + 1.0
+    ctr_x = boxes[:, :, 0] + 0.5 * length
 
-    dx = deltas[:, :, 0::4]
-    dy = deltas[:, :, 1::4]
-    dw = deltas[:, :, 2::4]
-    dh = deltas[:, :, 3::4]
+    dx = deltas[:, :, 0::2]
+    dw = deltas[:, :, 1::2]
 
-    pred_ctr_x = dx * widths.unsqueeze(2) + ctr_x.unsqueeze(2)
-    pred_ctr_y = dy * heights.unsqueeze(2) + ctr_y.unsqueeze(2)
-    pred_w = torch.exp(dw) * widths.unsqueeze(2)
-    pred_h = torch.exp(dh) * heights.unsqueeze(2)
+    pred_ctr_x = dx * length.unsqueeze(2) + ctr_x.unsqueeze(2)
+    pred_w = torch.exp(dw) * length.unsqueeze(2)
 
     pred_boxes = deltas.clone()
     # x1
-    pred_boxes[:, :, 0::4] = pred_ctr_x - 0.5 * pred_w
-    # y1
-    pred_boxes[:, :, 1::4] = pred_ctr_y - 0.5 * pred_h
+    pred_boxes[:, :, 0::2] = pred_ctr_x - 0.5 * pred_w
     # x2
-    pred_boxes[:, :, 2::4] = pred_ctr_x + 0.5 * pred_w
-    # y2
-    pred_boxes[:, :, 3::4] = pred_ctr_y + 0.5 * pred_h
+    pred_boxes[:, :, 1::2] = pred_ctr_x + 0.5 * pred_w
 
     return pred_boxes
 
-def clip_boxes_batch(boxes, im_shape, batch_size):
-    """
-    Clip boxes to image boundaries.
-    """
-    num_rois = boxes.size(1)
-
-    boxes[boxes < 0] = 0
-    # batch_x = (im_shape[:,0]-1).view(batch_size, 1).expand(batch_size, num_rois)
-    # batch_y = (im_shape[:,1]-1).view(batch_size, 1).expand(batch_size, num_rois)
-
-    batch_x = im_shape[:, 1] - 1
-    batch_y = im_shape[:, 0] - 1
-
-    boxes[:,:,0][boxes[:,:,0] > batch_x] = batch_x
-    boxes[:,:,1][boxes[:,:,1] > batch_y] = batch_y
-    boxes[:,:,2][boxes[:,:,2] > batch_x] = batch_x
-    boxes[:,:,3][boxes[:,:,3] > batch_y] = batch_y
-
-    return boxes
-
-def clip_boxes(boxes, im_shape, batch_size):
+# If the coordinate is set to over the total sequence length, clip it.
+def clip_boxes(boxes, im_shape, batch_size): # im_shape : (length, ch)
 
     for i in range(batch_size):
-        boxes[i,:,0::4].clamp_(0, im_shape[i, 1]-1)
-        boxes[i,:,1::4].clamp_(0, im_shape[i, 0]-1)
-        boxes[i,:,2::4].clamp_(0, im_shape[i, 1]-1)
-        boxes[i,:,3::4].clamp_(0, im_shape[i, 0]-1)
+        boxes[i,:,0::2].clamp_(0, im_shape[i, 0]-1)
+        boxes[i,:,1::2].clamp_(0, im_shape[i, 0]-1)
 
     return boxes
 
